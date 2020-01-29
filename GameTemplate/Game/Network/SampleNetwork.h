5400237e9e36ac29c7b6cd65f222ef9cc10c9727
kbc19b07 <kbc19b07@stu.kawahara.ac.jp>
2020-01-29 10:57:32 +0900

    通信の作成はじめました

---
+++ GameTemplate/Game/Network/SampleNetwork.h
@@ -0,0 +1,20 @@
+//#pragma once
+//#include "Photon/Common-cpp/inc/JString.h"
+//#include "Photon/LoadBalancing-cpp/inc/client.h"
+//#include "Photon/LoadBalancing-cpp/inc/listener.h"
+//#include "Photon/Common-cpp/inc/Logger.h"
+//
+//class SampleNetwork
+//{
+//public:
+//	SampleNetwork(const ExitGames::Common::JString& appID, const ExitGames::Common::JString& appVersion);
+//	void connect(void);
+//	void disconnect(void);
+//	void run(void);
+//
+//private:
+//	ExitGames::LoadBalancing::Client m_LoadBalancingClient;
+//	ExitGames::LoadBalancing::Listener m_Listener;
+//	ExitGames::Common::Logger m_Logger;
+//};
+//
