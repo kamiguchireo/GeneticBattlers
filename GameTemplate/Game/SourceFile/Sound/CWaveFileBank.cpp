6f4716ab292af5de4f534864804a43df97fc0072
kbc19b07 <kbc19b07@stu.kawahara.ac.jp>
2020-02-11 15:15:24 +0900

    サウンド関係のコンパイルが通った(音が出るかはわからん)

--- GameTemplate/Game/SourceFile/Sound/CWaveFileBank.cpp
+++ GameTemplate/Game/SourceFile/Sound/CWaveFileBank.cpp
@@ -1,2 +1,37 @@
 #include "stdafx.h"
+#include "WaveFile.h"
 #include "CWaveFileBank.h"
+#include "util/Util.h"
+
+namespace Engine {
+	CWaveFileBank::CWaveFileBank()
+	{
+	}
+	CWaveFileBank::~CWaveFileBank()
+	{
+		ReleaseAll();
+	}
+	void CWaveFileBank::Release(int groupID)
+	{
+		for (auto waveFile : m_waveFileMap[groupID]) {
+			waveFile.second->Release();
+		}
+		m_waveFileMap[groupID].clear();
+	}
+	void CWaveFileBank::UnregistWaveFile(int groupID, CWaveFilePtr waveFile)
+	{
+		m_waveFileMap->erase(waveFile->GetFilePathHash());
+	}
+	void CWaveFileBank::RegistWaveFile(int groupID, CWaveFilePtr waveFile)
+	{
+		m_waveFileMap[groupID].insert({ waveFile->GetFilePathHash(), waveFile });
+	}
+	CWaveFilePtr CWaveFileBank::FindWaveFile(int groupID, const wchar_t* filePath)
+	{
+		auto value = m_waveFileMap[groupID].find(Util::MakeHash(filePath));
+		if (value != m_waveFileMap[groupID].end()) {
+			return value->second;
+		}
+		return CWaveFilePtr();
+	}
+}
