95bf94b302bb6a6203cec19def74a91a0ab9c299
kbc19b07 <kbc19b07@stu.kawahara.ac.jp>
2020-02-28 10:38:12 +0900

    mainの中身をGameに移動した

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -16,33 +16,67 @@ Game::~Game()
 
 bool Game::Start()
 {
-	//m_fade = Fade::GetInstance();
-	//m_fade->StartFadeIn();
-
-	//スプライトの確認のためのポインタ
-	prefab::SpriteRender*m_spriteRender;
-
 	//スプライトを初期化
-	m_spriteRender = NewGO<prefab::SpriteRender>(0, "sprite");
-	m_spriteRender->Init(L"Assets/sprite/mikyan.dds", 400, 300);
-
+	//m_spriteRender = NewGO<prefab::SpriteRender>(0,"sprite");
+	//m_spriteRender->Init(L"Assets/sprite/mikyan.dds", 400, 300);
 	
+	//エフェクトを再生。
+	//effect->Play(L"Assets/effect/test.efk");
+	//CVector3 emitPos = CVector3::Zero();
+	//emitPos.y += 100.0f;
+	//effect->SetPosition(emitPos);
+	
+	//サウンドを再生
+	//ss.Init(L"Assets/sound/bgm_00.wav");
+	//ss.Play(false);
+
+	//モデル1
+	m_model.Init(L"Assets/modelData/unityChan.cmo");
+	CVector3 m_pos = { 0.0f,0.0f,100.0f };
+	m_model.UpdateWorldMatrix(m_pos, CQuaternion::Identity(), CVector3::One());
+	CQuaternion m_ligdir2 = { 1.0f,0.0f,0.0f,0.0f };
+	m_model.SetLightDir(m_ligdir2);
+	//m_model.SetActiveDLFlag(1);
+	m_model.SetActiveRLFlag(0);
+
+	//モデル2
+	m_model2.Init(L"Assets/modelData/unityChan.cmo");
+	CVector3 m_pos2 = { -100.0f,0.0f,100.0f };
+	m_model2.UpdateWorldMatrix(m_pos2, CQuaternion::Identity(), CVector3::One());
+	CVector4 m_ligCol = { 1.0f,1.0f,1.0f,1.0f };
+	CQuaternion m_ligdir = { 0.707f,0.0f,1.0f,1.0f};
+	m_model2.SetLightColor(m_ligCol);
+	m_model2.SetLightDir(m_ligdir);
+	m_model2.SetActiveDLFlag(1);
+
 	return true;
 }
 
 void Game::Update()
-{
-	if (g_pad[0].IsTrigger(enButtonA)
-		&& m_player == nullptr)
-	{
-		m_player = NewGO<Player>(0);
-		//m_fade->StartFadeIn
-	}
-	if (g_pad[0].IsTrigger(enButtonB)
-		&& m_player != nullptr)
-	{
-		DeleteGO(m_player);
-		m_player = nullptr;
-		//m_fade->StartFadeOut();
-	}
+{	
+	//モデルのドロー
+	m_model.Draw
+	(
+		g_camera3D.GetViewMatrix(),
+		g_camera3D.GetProjectionMatrix()
+	);
+
+	//モデル2のドロー
+	m_model2.Draw
+	(
+		g_camera3D.GetViewMatrix(),
+		g_camera3D.GetProjectionMatrix()
+	);
+
+	//if(GetAsyncKeyState(VK_UP))
+	//{
+		//エフェクトをNewGO
+		//effect = NewGO<prefab::CEffect>(0);
+		////エフェクトを再生。
+		//effect->Play(L"Assets/effect/test.efk");
+		//CVector3 emitPos = CVector3::Zero();
+		//emitPos.y += 100.0f;
+		//effect->SetPosition(emitPos);
+		//effect->SetScale({ 10.0f,10.0f,10.0f });
+	//}
 }
--- GameTemplate/Game/main.cpp
+++ GameTemplate/Game/main.cpp
@@ -5,11 +5,6 @@
 #include "Game.h"
 #include "Fade.h"
 #include "gameScenes/TitleScene.h"
-#include "SpriteRender.h"
-#include "ShaderResourceView.h"
-#include "SourceFile/graphic/2D/Sprite.h"
-#include "SourceFile/graphic/Effect/CEffect.h"
-#include "SourceFile/Sound/CSoundSource.h"
 #include "SourceFile/Sound/SoundEngine.h"
 #include "SourceFile/Light/DirectionLight.h"
 
@@ -29,68 +24,23 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	g_camera2D.SetPosition({ 0.0f, 0.0f, 0.0f });
 	g_camera2D.SetTarget({ 0.0f, 0.0f, 10.0f });
 	g_camera2D.SetFar(100.0f);
-	//CGameObjectManager gameObject;
-
-	//エフェクサーマネージャーの初期化
-	g_graphicsEngine->managerInit();
 	
-	//ゲームクラスの生成。
+	//Gameの生成。
+	//エンジンの実験してるので中身は触らないで
 	//NewGO<Game>(0, nullptr);
+
+	//ここに必要なものはNewGOしていってください
 	NewGO<TitleScene>(0, nullptr);
 	NewGO<Fade>(1, "Fade");
 	
-	////スプライトの確認のためのポインタ
-	//prefab::SpriteRender*m_spriteRender;
+	//エフェクサーマネージャーの初期化
+	//コメントアウトしないで
+	g_graphicsEngine->managerInit();
 
-	////スプライトを初期化
-	//m_spriteRender = NewGO<prefab::SpriteRender>(0,"sprite");
-	//m_spriteRender->Init(L"Assets/sprite/mikyan.dds", 400, 300);
-	//const float FRAME_BUFFER_W = 1280.0;				//フレームバッファの幅。
-	//const float FRAME_BUFFER_H = 720.0f;				//フレームバッファの高さ。
-
-	//ShaderResourceView m_srv;
-	//Sprite sprite;
-	//m_srv.CreateFromDDSTextureFromFile(L"Assets/sprite/win.dds");
-	//sprite.Init(m_srv.GetBody(), 500.0f, 500.0f);
-	//sprite.Update({0.0,0.0,5.0}, CQuaternion::Identity(), CVector3::One());
-
-	////エフェクトを作成。
-	//prefab::CEffect* effect = NewGO<prefab::CEffect>(0);
-	////エフェクトを再生。
-	//effect->Play(L"Assets/effect/test.efk");
-	//CVector3 emitPos = CVector3::Zero();
-	//emitPos.y += 100.0f;
-	//effect->SetPosition(emitPos);
-
-	//CSoundEngine se;
-	//se.Init();
-
-	//prefab::CSoundSource ss;
-	////ss = NewGO<prefab::CSoundSource>(0);
-	//ss.Init(L"Assets/sound/bgm_00.wav");
-	//ss.Play(false);
-
-	//SkinModel m_model;
-	//m_model.Init(L"Assets/modelData/unityChan.cmo");
-	//CVector3 m_pos = { 0.0f,0.0f,100.0f };
-	//m_model.UpdateWorldMatrix(m_pos, CQuaternion::Identity(), CVector3::One());
-	//CQuaternion m_ligdir2 = { 1.0f,0.0f,0.0f,0.0f };
-	//m_model.SetLightDir(m_ligdir2);
-	////m_model.SetActiveDLFlag(1);
-	//m_model.SetActiveRLFlag(0);
-
-	//SkinModel m_model2;
-	//m_model2.Init(L"Assets/modelData/unityChan.cmo");
-	//CVector3 m_pos2 = { -100.0f,0.0f,100.0f };
-	//m_model2.UpdateWorldMatrix(m_pos2, CQuaternion::Identity(), CVector3::One());
-	//CVector4 m_ligCol = { 1.0f,1.0f,1.0f,1.0f };
-	//CQuaternion m_ligdir = { 0.707f,0.0f,1.0f,1.0f};
-	//m_model2.SetLightColor(m_ligCol);
-	//m_model2.SetLightDir(m_ligdir);
-	//m_model2.SetActiveDLFlag(1);
-
-	//prefab::DirectionLight* DL;
-	//DL = NewGO<prefab::DirectionLight>(0);
+	//サウンド関係の初期化
+	//コメントアウトしないで
+	CSoundEngine se;
+	se.Init();
 
 	g_camera2D.Update2D();
 	
@@ -111,39 +61,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 		GameObjectManager().Update();
 		//カメラの更新。
 		g_camera3D.Update();
-		//sprite.Update({ 0.0,0.0,5.0 }, CQuaternion::Identity(), CVector3::One());
-		//sprite.Draw
-		//(
-		//	g_camera2D.GetViewMatrix(),
-		//	g_camera2D.GetProjectionMatrix()
-		//);
-		//g_graphicsEngine->Execute2DDraw();
 
-		//if(GetAsyncKeyState(VK_UP))
-		//{
 
-		//	//エフェクトを作成。
-		//	prefab::CEffect* effect = NewGO<prefab::CEffect>(0);
-		//	//エフェクトを再生。
-		//	effect->Play(L"Assets/effect/test.efk");
-		//	CVector3 emitPos = CVector3::Zero();
-		//	emitPos.y += 100.0f;
-		//	effect->SetPosition(emitPos);
-		//	effect->SetScale({ 10.0f,10.0f,10.0f });
-		//}
-
-		//モデルのドロー
-		//m_model.Draw
-		//(
-		//	g_camera3D.GetViewMatrix(),
-		//	g_camera3D.GetProjectionMatrix()
-		//);
-
-		//m_model2.Draw
-		//(
-		//	g_camera3D.GetViewMatrix(),
-		//	g_camera3D.GetProjectionMatrix()
-		//);
 		//描画終了。
 		g_graphicsEngine->EndRender();
 	}
