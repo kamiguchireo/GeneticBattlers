26bc1b9c5e93771a2cc1b6d0dcac745c2ee48205
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2020-08-06 12:22:52 +0900

    デバフの効果時間の減る処理忘れてた

--- GameTemplate/Game/monster/parameter/StatusManager.cpp
+++ GameTemplate/Game/monster/parameter/StatusManager.cpp
@@ -156,7 +156,7 @@ bool StatusManager::AddATB()
 	const float addTime = g_gameTime.GetFrameDeltaTime() * 2.0f;
 	//死亡時は処理を中断する。
 	if (m_stateAI == en_state_Death) return false;
-
+	//バフの効果時間減少。
 	m_activeTime += (float)m_status.DEX * addTime;
 	for (int i = 0; i < en_buff_num; i++) {
 		if (m_buffTimeList[i] == 0.0f) continue;
@@ -164,6 +164,15 @@ bool StatusManager::AddATB()
 		m_buffTimeList[i] -= addTime;
 		if (m_buffTimeList[i] < 0.0f) {
 			ResetBuff(i);
+		}
+	}
+	//デバフの効果時間減少。
+	for (int i = 0; i < en_buff_num; i++) {
+		if (m_debuffTimeList[i] == 0.0f) continue;
+
+		m_debuffTimeList[i] -= addTime;
+		if (m_debuffTimeList[i] < 0.0f) {
+			ResetDebuff(i);
 		}
 	}
 
